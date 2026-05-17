3a080e3797467b6b5e620fc86e2694cc152ea325
Lio Exp <liocosta07@gmail.com>
2026-05-17 13:55:55 +0100

    feat: initial commit

---
+++ target/debug/deps/liteml-ade865982b8cc829.d
@@ -0,0 +1,5 @@
+/home/lio/Documents/Area_Dev/Projetos/LiteML/target/debug/deps/liteml-ade865982b8cc829.d: src/main.rs
+
+/home/lio/Documents/Area_Dev/Projetos/LiteML/target/debug/deps/liteml-ade865982b8cc829: src/main.rs
+
+src/main.rs:
---
+++ target/debug/liteml.d
@@ -0,0 +1,1 @@
+/home/lio/Documents/Area_Dev/Projetos/LiteML/target/debug/liteml: /home/lio/Documents/Area_Dev/Projetos/LiteML/src/main.rs
